60a7a0f30d33a36ee314f6296f587635926a217d
Catarina Rato <catirato@gmail.com>
2026-03-10 22:05:49 +0000

    migrations

--- MusicBands_CatarinaRato/start.sh
+++ MusicBands_CatarinaRato/start.sh
@@ -8,6 +8,7 @@ sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
 sed -i "s/<VirtualHost \\*:80>/<VirtualHost *:${PORT}>/" /etc/apache2/sites-available/000-default.conf
 
 php artisan optimize:clear
+php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
