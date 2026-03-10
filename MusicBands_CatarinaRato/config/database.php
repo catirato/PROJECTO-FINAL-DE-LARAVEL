88f3907aac517a18d87e276996860b2e8e5766a7
Catarina Rato <catirato@gmail.com>
2026-03-10 20:20:44 +0000

    Keep MySQL SSL verify option in database config

--- MusicBands_CatarinaRato/config/database.php
+++ MusicBands_CatarinaRato/config/database.php
@@ -61,7 +61,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
                 (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_VERIFY_SERVER_CERT : \PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT) => filter_var(env('MYSQL_ATTR_SSL_VERIFY_SERVER_CERT', false), FILTER_VALIDATE_BOOL),
-            ]) : [],
+            ], fn ($value) => !is_null($value)) : [],
         ],
 
         'mariadb' => [
