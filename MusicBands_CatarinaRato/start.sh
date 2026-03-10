dfed2d70a1e1788a32a281f45860ba5620665fc4
Catarina Rato <catirato@gmail.com>
2026-03-10 19:35:20 +0000

    Update start.sh

--- MusicBands_CatarinaRato/start.sh
+++ MusicBands_CatarinaRato/start.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-php artisan config:clear
-php artisan cache:clear
-
+php artisan optimize:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-
-ls -l /etc/secrets && php -r "var_dump(file_exists('/etc/secrets/ca.pem'));"
 
 apache2-foreground
